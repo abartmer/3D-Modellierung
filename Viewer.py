2c61e575c29ef328f4547eba6504af3707b8c264
buddy <abartmer@students.uni-mainz.de>
2018-11-04 17:44:37 +0100

    polygons class parameter added: n = size of polygon

--- Viewer.py
+++ Viewer.py
@@ -35,36 +35,52 @@ class Viewer(qw.QMainWindow):
         self.display.setPixmap(self.canvas)
 
     def draw_polygon_cav(self):
-        a = 200
-        b = 500
-        c = 60
-        d = 120
-        cube = [(a, a, d), (a, b, d), (b, b, d), (b, a, d),
-                (a, a, c), (a, a, d), (b, a, d), (b, a, c),
-                (a, a, c), (a, b, c), (a, b, d), (a, a, d),
-                (a, a, c), (a, b, d), (b, b, d), (b, b, c),
-                (b, a, d), (b, b, d), (b, b, c), (b, a, c),
-                (a, a, d), (a, b, c), (b, b, d), (b, a, c)]
-        cube_obj = Polygon(*cube)
 
         self.painter = qg.QPainter(self.canvas)
         self.painter.setPen(qg.QPen(qc.Qt.red, 5, qc.Qt.SolidLine))
         self.painter.setBrush(qg.QBrush(qc.Qt.red, qc.Qt.SolidPattern))
 
-        for point in cube_obj.points:
-            x = point[0] + 0.5 * np.sqrt(2) * point[2]
-            y = point[1] + 0.5 * np.sqrt(2) * point[2]
+        for poly in cube_obj.polygons:
+            x = poly[0][0] + 0.5 * np.sqrt(2) * poly[0][2]
+            y = poly[0][1] + 0.5 * np.sqrt(2) * poly[0][2]
             self.painter.drawPoint(x, y)
 
-        for point in range(len(cube_obj.points) - 1):
-            x = cube_obj.points[point][0] + 0.5 * np.sqrt(2) * cube_obj.points[point][2]
-            y = cube_obj.points[point][1] + 0.5 * np.sqrt(2) * cube_obj.points[point][2]
-            x2 = cube_obj.points[point + 1][0] + 0.5 * np.sqrt(2) * cube_obj.points[point + 1][2]
-            y2 = cube_obj.points[point + 1][1] + 0.5 * np.sqrt(2) * cube_obj.points[point + 1][2]
-            self.painter.drawLine(x, y, x2, y2)
-
+        # attempting to draw a line from starting point to the following point (start = end)
+        for i in cube_obj.polygons:
+            for poly in range(len(cube_obj.polygons) - 1):
+                x1 = cube_obj.polygons[i][poly][0]
+                x2 = cube_obj.polygons[i][poly + 1][0]
+                y1 = cube_obj.polygons[i][poly][1]
+                y2 = cube_obj.polygons[i][poly + 1][1]
+                self.painter.drawLine(x1, y1, x2, y2)
         self.display.setPixmap(self.canvas)
 
+        print(cube_obj.polygons[0][0][0])
+
+
+"""     for poly in range(len(cube_obj.polygons) - 1):
+            x1 = (cube_obj.polygons[poly][0][0] + 0.5 * np.sqrt(2) * cube_obj.polygons[poly][0][2])
+            y1 = (cube_obj.polygons[poly][0][1] + 0.5 * np.sqrt(2) * cube_obj.polygons[poly][0][2])
+            x2 = (cube_obj.polygons[poly][1][0] + 0.5 * np.sqrt(2) * cube_obj.polygons[poly][1][2])
+            y2 = (cube_obj.polygons[poly][1][1] + 0.5 * np.sqrt(2) * cube_obj.polygons[poly][1][2])
+            self.painter.drawLine(x1, y1, x2, y2)"""
+
+
+
+
+
+
+a = 200
+b = 500
+c = 60
+d = 120
+cube = [(a, a, d), (a, b, d), (b, b, d), (b, a, d),
+        (a, a, c), (a, a, d), (b, a, d), (b, a, c),
+        (a, a, c), (a, b, c), (a, b, d), (a, a, d),
+        (a, a, c), (a, b, d), (b, b, d), (b, b, c),
+        (b, a, d), (b, b, d), (b, b, c), (b, a, c),
+        (a, a, d), (a, b, c), (b, b, d), (b, a, c)]
+cube_obj = Polygon(4, *cube)
 
 # Start app
 if __name__ == '__main__':
